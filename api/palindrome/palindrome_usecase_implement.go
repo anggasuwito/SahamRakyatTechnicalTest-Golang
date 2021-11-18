a20197ea78013ebab1164e3bdc1f7b1726ccdd0e
Angga Suwito <angga.q.suwito@gmail.com>
2021-11-18 15:23:10 +0700

    #first commit

--- api/palindrome/palindrome_usecase_implement.go
+++ api/palindrome/palindrome_usecase_implement.go
@@ -2,35 +2,16 @@ package palindrome
 
 import (
 	"SahamRakyatTechnicalTest-Golang/api/model"
-	"fmt"
 	"strings"
 )
 
 func (l PalindromeUsecase) LongestPalindrome(body model.Palindrome) string {
-	maxLen := len(body.Word)
-
-	for i := maxLen; i > 0; i-- {
-		var wordA string
-		var wordB string
-		for j := 0; j+i <= maxLen; j++ {
-			tempA := j
-			tempB := j + i - 1
-			wordA = strings.ToLower(fmt.Sprintf("%s", body.Word[tempA]))
-			wordB = strings.ToLower(fmt.Sprintf("%s", body.Word[tempB]))
-
-			for tempA < tempB && wordA == wordB {
-				tempA++
-				tempB--
-			}
-
-			if tempA >= tempB {
-				return body.Word[j : j+i]
-			}
-		}
-		if wordA == " " || wordB == " " {
-			break
+	splittedWord := strings.Split(body.Word, "")
+	for i := 0; i <= len(splittedWord)-1; i++ {
+		if splittedWord[i] != splittedWord[len(splittedWord)-(i+1)] {
+			return "Its Not Palindrome"
 		}
 	}
 
-	return body.Word[0:1]
+	return "Palindrome"
 }
