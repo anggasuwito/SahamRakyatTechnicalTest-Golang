c7fbc098e48c8c956d5328b365405a0a35fd1ad3
Angga Suwito <angga.q.suwito@gmail.com>
2021-11-19 09:35:54 +0700

    #palindrome

--- api/palindrome/palindrome_usecase_implement.go
+++ api/palindrome/palindrome_usecase_implement.go
@@ -5,14 +5,33 @@ import (
 	"strings"
 )
 
-//Belum sesuai soal
 func (l PalindromeUsecase) LongestPalindrome(body model.Palindrome) string {
-	splittedWord := strings.Split(body.Word, "")
-	for i := 0; i <= len(splittedWord)-1; i++ {
-		if splittedWord[i] != splittedWord[len(splittedWord)-(i+1)] {
-			return "Its Not Palindrome"
+	var result string
+	var current string
+	removedSpace := strings.ReplaceAll(body.Word, " ", "")
+	for i := 0; i < len(removedSpace); i++ {
+		odd := expandFromCenter(removedSpace, i, i)
+		even := expandFromCenter(removedSpace, i, i+1)
+
+		if len(odd) > len(even) {
+			current = odd
+		} else {
+			current = even
+		}
+
+		if len(current) > len(result) {
+			result = current
 		}
 	}
 
-	return "Palindrome"
+	return result
+}
+
+func expandFromCenter(word string, left int, right int) string {
+	for left >= 0 && right < len(word) && word[left] == word[right] {
+		left -= 1
+		right += 1
+	}
+
+	return word[left+1 : right]
 }
